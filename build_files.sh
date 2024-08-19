e65cee821df5977eea3836439ca603a00a93e50c
phani69015 <2100069015eee@gmail.com>
2024-08-19 12:20:51 +0530

    trying to deploy attempt 2

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,13 @@
-@echo off
+#!/bin/bash
+
+# Create a virtual environment
+python3.9 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Run Django collectstatic
+python3.9 manage.py collectstatic --noinput
