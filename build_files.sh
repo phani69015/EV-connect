1a9c15b8fb4addf17f943635ef43fabdd6e1651e
phani69015 <2100069015eee@gmail.com>
2024-08-19 20:36:47 +0530

    railway deploy attempt 1

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,28 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+
+# Set up virtual environment
 python3 -m venv venv
+
+# Activate virtual environment
 source venv/bin/activate
+
+# Upgrade pip to the latest version
+pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run database migrations
+python manage.py migrate --noinput
+
+# Other commands (if needed)
+# e.g., python manage.py test
+
+# Deactivate virtual environment
+deactivate
